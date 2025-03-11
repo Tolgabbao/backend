7dee4d3e14b7b603e06b88f1ea0a47f7e4eb7229
Alaz Arslan <alazarslan5@gmail.com>
2025-03-11 11:21:47 +0300

    added login unit test with missing password and missing email

--- accounts/tests.py
+++ accounts/tests.py
@@ -40,6 +40,7 @@ class UserAuthenticationTest(TestCase):
         self.assertEqual(response_data["username"], self.user_data['username'])
         self.assertTrue(response.wsgi_request.user.is_authenticated)  # user oturum açmış mı kontrol eder
 
+
     """Test login with non-existent user"""
     def test_login_nonexistent_user(self):
         # wrong email #
@@ -78,6 +79,50 @@ class UserAuthenticationTest(TestCase):
         response_data = response.json() # gelen HTTP yanıtının gövdesindeki veriyi
                                         # JSON formatında çözümleyerek Python dictionary
                                         # olarak döndürür
+        self.assertEqual(response_data['message'], 'Invalid credentials')
+        self.assertFalse(response.wsgi_request.user.is_authenticated)  # user oturum açmamış mı kontrol eder
+
+    """Test login with missing password"""
+    def test_login_missing_password(self):
+        #Creating user
+        user = User.objects.create_user(username=self.user_data['username'],
+                                        email=self.user_data['email'],
+                                        password=self.user_data['password'])
+
+        missing_data = {
+            'email': 'test@example.com',
+            #missing password
+        }
+
+        response = self.client.post( self.login_url, missing_data)
+        self.assertEqual(response.status_code, 400)
+
+        response_data = response.json() # gelen HTTP yanıtının gövdesindeki veriyi
+                                        # JSON formatında çözümleyerek Python dictionary
+                                        # olarak döndürür
+
+        self.assertEqual(response_data['message'], 'Invalid credentials')
+        self.assertFalse(response.wsgi_request.user.is_authenticated)  # user oturum açmamış mı kontrol eder
+
+    """Test login with missing email"""
+    def test_login_missing_email(self):
+        #Creating user
+        user = User.objects.create_user(username=self.user_data['username'],
+                                        email=self.user_data['email'],
+                                        password=self.user_data['password'])
+
+        missing_data = {
+            #missing email
+            'password': "testpassword"
+        }
+
+        response = self.client.post( self.login_url, missing_data)
+        self.assertEqual(response.status_code, 400)
+
+        response_data = response.json() # gelen HTTP yanıtının gövdesindeki veriyi
+                                        # JSON formatında çözümleyerek Python dictionary
+                                        # olarak döndürür
+
         self.assertEqual(response_data['message'], 'Invalid credentials')
         self.assertFalse(response.wsgi_request.user.is_authenticated)  # user oturum açmamış mı kontrol eder
 
