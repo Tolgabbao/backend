348d4dfc05ad5c72e7142977436c2358b8036ca5
Tolga Toker <76428491+Tolgabbao@users.noreply.github.com>
2025-03-11 11:26:03 +0300

    Email should be required

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     )
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = ["username", "email"]
     user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
     address = models.TextField()
     phone = models.CharField(max_length=15, blank=True)
