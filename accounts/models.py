c3cf4cc7fcddbb352e119a9e243bede086f44584
erencebeci <eren.cebeci@sabanciuniv.edu>
2025-03-21 11:11:27 +0300

    Added Address model with main address handling and ordering

--- accounts/models.py
+++ accounts/models.py
@@ -34,3 +34,71 @@ class User(AbstractUser):
 
     def is_product_manager(self):
         return self.user_type == "PRODUCT_MANAGER"
+
+    def get_main_address(self):
+        try:
+            return self.addresses.get(is_main=True)
+        except Address.DoesNotExist:
+            # Return the first address or None if no addresses exist
+            return self.addresses.first()
+
+    def get_addresses(self):
+        """Return a list of address dictionaries"""
+        addresses = []
+        for address in self.addresses.all():
+            addresses.append({
+                "id": address.id,
+                "name": address.name,
+                "street_address": address.street_address,
+                "city": address.city,
+                "state": address.state,
+                "postal_code": address.postal_code,
+                "country": address.country,
+                "is_main": address.is_main
+            })
+        return addresses
+
+    def get_main_address_dict(self):
+        """Return the main address as a dictionary"""
+        main_address = self.get_main_address()
+        if main_address:
+            return {
+                "id": main_address.id,
+                "name": main_address.name,
+                "street_address": main_address.street_address,
+                "city": main_address.city,
+                "state": main_address.state,
+                "postal_code": main_address.postal_code,
+                "country": main_address.country,
+                "is_main": main_address.is_main
+            }
+        return None
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    name = models.CharField(max_length=100)  # E.g., "Home", "Work", etc.
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    is_main = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+        ordering = ['-is_main', '-updated_at']  # Main address first, then most recently updated
+
+    def __str__(self):
+        return f"{self.name} - {self.street_address}, {self.city}"
+
+    def save(self, *args, **kwargs):
+        # If this address is being set as main, unset is_main for all other addresses of this user
+        if self.is_main:
+            Address.objects.filter(user=self.user, is_main=True).update(is_main=False)
+        # If this is the first address for the user, make it the main address
+        elif not self.pk and not Address.objects.filter(user=self.user).exists():
+            self.is_main = True
+        super().save(*args, **kwargs)
